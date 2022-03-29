a8f857aa5b82cf63eb4d9b60b3b752290cc55502
hyojeong <vlsjt3@naver.com>
2022-03-29 16:45:45 +0900

    section-06 Configuration, Bean

--- src/main/java/moviebuddy/MovieBuddyFactory.java
+++ src/main/java/moviebuddy/MovieBuddyFactory.java
@@ -1,17 +1,37 @@
 package moviebuddy;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Scope;
 
 import moviebuddy.domain.CsvMovieReader;
 import moviebuddy.domain.MovieFinder;
+import moviebuddy.domain.MovieReader;
 
 @Configuration
+@Import({ MovieBuddyFactory.DomainModuleConfig.class, MovieBuddyFactory.DataSourceModuleConfig.class })
 public class MovieBuddyFactory {
-	
-	@Bean
-	public MovieFinder movieFinder() {
-		return new MovieFinder(new CsvMovieReader());
-	}
 
+	@Configuration
+	static class DomainModuleConfig {
+		
+		@Bean
+		//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+		public MovieFinder movieFinder(MovieReader movieReader) {
+			return new MovieFinder(movieReader);
+		}
+
+	}
+	
+	@Configuration
+	static class DataSourceModuleConfig {
+		
+		@Bean
+		public MovieReader movieReader() {
+			return new CsvMovieReader();
+		}
+	}
+	
 }
