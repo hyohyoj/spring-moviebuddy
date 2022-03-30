e7d039ae452185f20208722fec7c2de247310b1d
hyojeong <vlsjt3@naver.com>
2022-03-30 10:38:50 +0900

    section-08 스프링 통합 테스트 지원

--- src/test/java/moviebuddy/domain/JaxbMovieReaderTest.java
+++ src/test/java/moviebuddy/domain/JaxbMovieReaderTest.java
@@ -4,16 +4,26 @@ import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
+import moviebuddy.MovieBuddyFactory;
+
+
+@SpringJUnitConfig(MovieBuddyFactory.class)
 public class JaxbMovieReaderTest {
+	
+	JaxbMovieReader movieReader;
+	
+	@Autowired
+	JaxbMovieReaderTest(JaxbMovieReader movieReader) {
+		this.movieReader = movieReader;
+	}
 	
 	@Test
 	void NotEmpty_LoadedMovies() {
-		JaxbMovieReader movieReader = new JaxbMovieReader();
-		
 		List<Movie> movies = movieReader.loadMovies();
 		Assertions.assertEquals(1375, movies.size());
-		//MovieFinderTest.assertEquals(1375, movies.size());
 	}
 
 }
--- src/test/java/moviebuddy/domain/MovieFinderTest.java
+++ src/test/java/moviebuddy/domain/MovieFinderTest.java
@@ -4,19 +4,39 @@ import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import moviebuddy.MovieBuddyFactory;
 
 /**
  * @author springrunner.kr@gmail.com
  */
+@SpringJUnitConfig(MovieBuddyFactory.class)
+//@ExtendWith(SpringExtension.class)
+//@ContextConfiguration(classes = MovieBuddyFactory.class)
 public class MovieFinderTest {
 
-	final ApplicationContext applicationContext = 
-			new AnnotationConfigApplicationContext(MovieBuddyFactory.class);
-	final MovieFinder movieFinder = applicationContext.getBean(MovieFinder.class);
+	//필드에 Autowired
+	@Autowired
+	MovieFinder movieFinder;
+	
+	//생성자에 Autowired
+	@Autowired
+	MovieFinderTest(MovieFinder movieFinder) {
+		this.movieFinder = movieFinder;
+	}
+	
+	//세터에 Autowired
+	@Autowired
+	void setMovieFinder(MovieFinder movieFinder) {
+		this.movieFinder = movieFinder;
+	}
 	
 	@Test
 	void NotEmpty_directedBy() {
